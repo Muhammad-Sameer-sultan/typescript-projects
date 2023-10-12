6771a3ca3bc05f4046dc57f915d0bacc23cf819c
sameer <sameersultan1001@gmail.com>
2023-10-12 15:44:29 +0500

    word counter

---
+++ 06 word_counter/main.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+
+ const main= async()=>{
+    const {para}=await inquirer.prompt({
+        type:"input",
+        name: "para",
+        message: "Enter the paragraph"
+    }) 
+    const words = para.trim().split(/\s/).length;
+    const characters = para.replace(/\s/g,"").length;
+
+    // Split the input into words and count them
+  
+    console.log(`The total character is ${characters} and words ${words}`)
+}
+main();
