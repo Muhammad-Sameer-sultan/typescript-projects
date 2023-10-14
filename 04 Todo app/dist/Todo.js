25fa4a51002e7084303dc747f00c821e0a8cdf68
sameer <sameersultan1001@gmail.com>
2023-10-14 16:10:25 +0500

    bank

--- 04 Todo app/dist/Todo.js
+++
@@ -1,9 +0,0 @@
-// Todo.ts
-export default class Todo {
-    title;
-    completed;
-    constructor(title, completed = false) {
-        this.title = title;
-        this.completed = completed;
-    }
-}
