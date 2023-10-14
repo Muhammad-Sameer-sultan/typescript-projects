e564e1d010561dab6c9a3d4496674e1a80a33c89
sameer <sameersultan1001@gmail.com>
2023-10-14 15:50:02 +0500

    oops bank account

---
+++ 11 oops_mybamk/src/customerOptions.ts
@@ -0,0 +1,125 @@
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import { createSpinner } from 'nanospinner';
+
+const sleep = () => new Promise((r) => setTimeout(r, 2000));
+
+interface Customer {
+  name: string;
+  age: number;
+  contactNumber: string;
+  userId: string;
+  bankAccount: {
+    accountBalance: number;
+    accountNumber: string;
+    transactionHistory: Transaction[];
+    Credit(amount: number): void;
+    Debit(amount: number): void;
+  };
+}
+
+interface Transaction {
+  type: string;
+  amount: number;
+  date: string;
+  fee: number;
+}
+
+export function DisplayInfo(customer: any): void {
+  console.log(chalk.whiteBright(`--------------------------------------`));
+  console.log(`${chalk.bgRgb(1, 59, 52)(`Name            : `)} ${customer.name}`);
+  console.log(`${chalk.bgRgb(1, 59, 52)(`Age             : `)} ${customer.age}`);
+  console.log(`${chalk.bgRgb(1, 59, 52)(`Contact Number  : `)} ${customer.contactNumber}`);
+  console.log(`${chalk.bgRgb(1, 59, 52)(`UserID          : `)} ${customer.userId}`);
+  console.log(`${chalk.bgRgb(1, 59, 52)(`Account Balance : `)} RS: ${customer.bankAccount.accountBalance}`);
+  console.log(`${chalk.bgRgb(1, 59, 52)(`Account Number  : `)} ${customer.bankAccount.accountNumber}`);
+  console.log(chalk.whiteBright(`--------------------------------------`));
+}
+
+export function ShowAccountBalance(customer: any): void {
+  console.log(chalk.whiteBright(`--------------------------------------`));
+  console.log(`${chalk.bgRgb(1, 59, 52)(`Account Balance : `)} RS: ${customer.bankAccount.accountBalance}`);
+  console.log(chalk.whiteBright(`--------------------------------------`));
+}
+
+export async function Credit(customer: any): Promise<void> {
+  while (true) {
+    const { amount } = await inquirer.prompt([
+      {
+        name: 'amount',
+        message: 'Enter Amount : ',
+        type: 'number',
+      },
+    ]);
+
+    let spinner = createSpinner('Processing').start();
+    await sleep();
+
+    if (!amount) {
+      spinner.error({ text: chalk.whiteBright.bgRedBright(` Enter Correct Amount`) });
+      continue;
+    }
+
+    customer.bankAccount.Credit(amount);
+
+    spinner.success({
+      text: chalk.whiteBright.bgRgb(0, 125, 17)(`${amount > 100 ? 'Transaction Successful' : 'Transaction Successful And RS:1 Minus'} `),
+    });
+
+    return;
+  }
+}
+
+export async function Debit(customer: any): Promise<void> {
+  while (true) {
+    const { amount } = await inquirer.prompt([
+      {
+        name: 'amount',
+        message: 'Enter Amount : ',
+        type: 'number',
+      },
+    ]);
+
+    let spinner = createSpinner('Processing').start();
+    await sleep();
+
+    if (!amount) {
+      spinner.error({ text: chalk.whiteBright.bgRedBright(` Enter Correct Amount`) });
+      continue;
+    }
+
+    if (amount > customer.bankAccount.accountBalance) {
+      spinner.error({ text: chalk.whiteBright.bgRedBright(` Amount is Greater than Your Balance`) });
+      return;
+    }
+
+    customer.bankAccount.Debit(amount);
+
+    spinner.success({ text: chalk.whiteBright.bgRgb(0, 125, 17)(`Transaction Successful`) });
+
+    return;
+  }
+}
+
+export function TransactionHistory(customer: any): void {
+  if (!customer.bankAccount.transactionHistory.length) {
+    console.log(` No Transaction Available`);
+    return;
+  }
+
+  console.table(
+    customer.bankAccount.transactionHistory.map((val:any) => ({
+      ...val,
+      fee: `RS: ${val.fee}`,
+      amount: `RS: ${val.amount}`,
+    }))
+  );
+
+  // Alternatively, you can use forEach for custom formatting:
+  // customer.bankAccount.transactionHistory.forEach((val) => {
+  //     console.log(`\nTransaction Type   : ${val.type}`)
+  //     console.log(`Transaction Amount : RS: ${val.amount}`)
+  //     console.log(`Transaction Date   : ${val.date}`)
+  //     console.log(`Transaction Fee    : RS: ${val.fee}\n`)
+  // });
+}
---
+++ 11 oops_mybamk/src/main.ts
@@ -0,0 +1,193 @@
+#!/usr/bin/env nodeShowAccountBalance
+
+import inquirer from "inquirer";
+import { createSpinner } from "nanospinner";
+import chalk from "chalk";
+import { Customer } from "./classes/cutomer.js"; // Update the path to match your file structure
+import { DisplayInfo, ShowAccountBalance, Credit, Debit, TransactionHistory } from "./customerOptions.js";
+
+const sleep = () => new Promise((r) => setTimeout(r, 2000));
+
+console.log(chalk.bold.rgb(204, 204, 204)(`\n   <<<=================================>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`<<<===========>>>  ${chalk.redBright.bold('MY BANK')}  <<<===========>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`   <<<=================================>>>\n`));
+
+let customers: Customer[] = [];
+
+async function choice(): Promise<'C' | 'S'> {
+  const { option }: { option: 'C' | 'S' } = await inquirer.prompt([
+    {
+      name: "option",
+      message: 'What Would You Like To Do ?',
+      type: 'list',
+      choices: [{ name: 'Create New Account', value: 'C' }, { name: 'Sign In', value: 'S' }],
+    },
+  ]);
+  return option;
+}
+
+async function createNewAccount(): Promise<void> {
+  enum Names {
+    Name = 'Name',
+    Age = 'Age',
+    ContactNumber = 'Contact Number',
+    Pin = 'Pin',
+    UserID = 'UserID',
+  }
+
+  async function inputs(name: Names, type: string): Promise<string | number> {
+    while (true) {
+      const { input } = await inquirer.prompt([
+        {
+          name: 'input',
+          message: `Enter Your ${name} : `,
+          type: type as any, // Fix the type casting issue
+        },
+      ]);
+
+      if (!input) {
+        continue;
+      }
+
+      if (name === Names.ContactNumber) {
+        let numRegex = /^(\+92|0|92)[0-9]{10}$/;
+        if (!numRegex.test(input)) {
+          console.log(chalk.redBright(`  Use Pakistani Number`));
+          continue;
+        }
+      }
+
+      if (name === Names.UserID) {
+        let customer = customers.find((val) => val.userId === input);
+        if (customer) {
+          console.log(chalk.redBright(`  This UserID Is Already Taken Try Different`));
+          continue;
+        }
+      }
+
+      return input;
+    }
+  }
+
+  let name = (await inputs(Names.Name, 'string')) as string;
+  let age = (await inputs(Names.Age, 'number')) as number;
+  let contactNumber = (await inputs(Names.ContactNumber, 'string')) as string;
+  let pin = (await inputs(Names.Pin, 'number')) as number;
+  let userId = (await inputs(Names.UserID, 'string')) as string;
+  let customer = new Customer(name, age, contactNumber, pin, userId);
+  let spinner = createSpinner('Creating Account').start();
+  await sleep();
+  customers.push(customer);
+  spinner.success({ text: chalk.whiteBright.bgRgb(0, 125, 17)(' Account Created Successfully ') });
+}
+
+async function signIn(): Promise<void> {
+  const { userID, pin }: { userID: string; pin: number } = await inquirer.prompt([
+    {
+      name: 'userID',
+      message: "Enter Your UserID : ",
+    },
+    {
+      name: 'pin',
+      message: 'Enter Your Pin : ',
+      type: 'number',
+    },
+  ]);
+
+  let customer = customers.find((val) => val.userId === userID);
+
+  let spinner = createSpinner('Signing In').start();
+  await sleep();
+
+  if (!customer) {
+    spinner.error({ text: chalk.whiteBright.bgRed(` No Customer With This UserID`) });
+    return;
+  } else {
+    if (customer.pin !== pin) {
+      spinner.error({ text: chalk.whiteBright.bgRed(` Incorrect PIN`) });
+      return;
+    }
+
+    spinner.success({ text: chalk.whiteBright.bgRgb(0, 125, 17)('Signed In Successfully') });
+    console.log(chalk.whiteBright(`\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n`));
+
+    while (true) {
+      const {
+        userChoice,
+      }: {
+        userChoice: 'Show Profile' | 'Debit' | 'Credit' | 'Account Balance' | 'Transaction History';
+      } = await inquirer.prompt([
+        {
+          name: 'userChoice',
+          message: 'Make Your Choice',
+          type: 'rawlist',
+          choices: ['Show Profile', 'Debit', 'Credit', 'Account Balance', 'Transaction History'],
+        },
+      ]);
+
+      switch (userChoice) {
+        case 'Show Profile':
+          DisplayInfo(customer);
+          break;
+        case 'Account Balance':
+          ShowAccountBalance(customer);
+          break;
+        case 'Credit':
+          await Credit(customer);
+          break;
+        case 'Debit':
+          await Debit(customer);
+          break;
+        case 'Transaction History':
+          TransactionHistory(customer);
+          break;
+        default:
+          break;
+      }
+
+      const { choice }: { choice: 'Perform Another Task' | 'Sign Out' } = await inquirer.prompt([
+        {
+          name: 'choice',
+          message: 'Select One: ',
+          type: 'list',
+          choices: ['Perform Another Task', 'Sign Out'],
+        },
+      ]);
+
+      if (choice === 'Sign Out') {
+        console.log(chalk.whiteBright(`\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n`));
+        break;
+      } else {
+        console.log(chalk.whiteBright(`\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n`));
+        continue;
+      }
+    }
+  }
+}
+
+(async () => {
+  while (true) {
+    let userChoice = await choice();
+
+    if (userChoice === 'C') {
+      await createNewAccount();
+    } else if (userChoice === 'S') {
+      await signIn();
+    }
+
+    // EXIT PROGRAM CHOICE
+    const input = await inquirer.prompt([
+      {
+        name: chalk.rgb(255, 255, 160)(`Do You Want To Exit?`),
+        type: 'confirm',
+        default: false,
+      },
+    ]);
+    let value: boolean = await input['\x1B[38;2;255;255;160mDo You Want To Exit?\x1B[39m'];
+    if (value) {
+      break;
+    }
+    console.log(chalk.whiteBright('\n================================================================'));
+    console.log(chalk.whiteBright('================================================================\n'));
+  }
+})();
